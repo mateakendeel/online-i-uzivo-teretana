cb621483d4601d22e560ab4db0b32d86491f00ed
Matea Kenđel <matea.kendel@student.uniri.hr>
2024-11-26 16:40:24 +0100

    models-trener i korisnik

--- teretana_aplikacija/teretana/models.py
+++ teretana_aplikacija/teretana/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Trainer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
