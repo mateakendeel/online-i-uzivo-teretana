34c60b38eaf6db5282977a974fbefc16dbbb6550
Matea Kenđel <matea.kendel@student.uniri.hr>
2024-11-27 08:52:17 +0100

    models.py- Clanstvo i Vjezba

--- teretana_aplikacija/teretana/models.py
+++ teretana_aplikacija/teretana/models.py
@@ -20,7 +20,25 @@ class Trening(models.Model):
     trainer=models.ForeignKey(Trainer, on_delete=models.CASCADE)
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     date=models.DateTimeField()
+    
+    def __str__(self):
+        return f"{self.name}
+        with{self.trainer.name} for {self.user.username}"
+    
+class Clanstvo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    active = models.BooleanField(default=True)
 
-def_str_(self):
-return f"{self.name}
-with{self.trainer.name} for {self.user.username}
+    def __str__(self):
+        return f"Membership for {self.user.username} - {'Active' if self.active else 'Inactive'}"
+
+class Vjezba(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.IntegerField(help_text="Duration in minutes")
+    calories_burned = models.IntegerField(help_text="Estimated calories burned")
+
+    def __str__(self):
+        return self.name
