56fe881268ca7d423919697de0f1eff07c2eaa51
Matea Kenđel <matea.kendel@student.uniri.hr>
2024-12-12 08:55:25 +0100

    templates

--- teretana_aplikacija/teretana/views.py
+++ teretana_aplikacija/teretana/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user_group = Group.objects.get(name='User')  
+            user.groups.add(user_group)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
