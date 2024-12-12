7b2fb26f4fd4e18c4891b4f47e937372839b09cd
moiragrozdanicc <163437077+moiragrozdanicc@users.noreply.github.com>
2024-12-12 11:34:21 +0100

    Update urls.py

--- teretana_aplikacija/teretana_aplikacija/urls.py
+++ teretana_aplikacija/teretana_aplikacija/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from teretana import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
 ]
