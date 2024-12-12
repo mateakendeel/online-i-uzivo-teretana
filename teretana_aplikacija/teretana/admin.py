ea262cd57c2e606cedc4d8d9129dbfac9a80d655
moiragrozdanicc <163437077+moiragrozdanicc@users.noreply.github.com>
2024-12-12 11:32:17 +0100

    Update admin.py

--- teretana_aplikacija/teretana/admin.py
+++ teretana_aplikacija/teretana/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.unregister(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
